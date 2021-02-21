cb59410da844c78c5f9e2c39daac2e0f476cd6e8
Poompoomi <proulxlive@gmail.com>
2021-02-20 23:54:38 -0500

    added sql schema, see -simon -ahmed

---
+++ schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE athletes(
+	ID int,
+	name varchar(255),
+	date_of_birth date,
+	gender varchar(255),
+	PRIMARY KEY(id)
+);
+CREATE TABLE competitions (
+    ID int,
+    comp_name varchar(255),
+    venue varchar (255),
+    start_date_time timestamp,
+    end_date_time timestamp, 
+    PRIMARY KEY (ID)
+);
+CREATE TABLE register (
+    comp_id int,
+    athlete_id int, 
+    PRIMARY KEY (comp_id, athlete_id )
+);
