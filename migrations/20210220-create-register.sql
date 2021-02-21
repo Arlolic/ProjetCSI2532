4f1fbdf640e9b1459877d0c775c6fef481267301
Arlolic <ahcharba@gmail.com>
2021-02-21 17:01:54 -0500

    updated README.md and seed.sql

--- migrations/20210220-create-register.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE register (
-    comp_id int,
-    athlete_id int, 
-    PRIMARY KEY (comp_id, athlete_id )
-);
---
+++ migrations/202102201036-create-register.sql
@@ -0,0 +1,7 @@
+CREATE TABLE register (
+    comp_id int,
+    athlete_id int, 
+    PRIMARY KEY (comp_id, athlete_id ),
+    FOREIGN KEY (comp_id) REFERENCES competitions(ID),
+    FOREIGN KEY (athlete_id) REFERENCES athletes(ID)
+);
