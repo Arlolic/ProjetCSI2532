baaed8e201be5c2361888038532f66c911b7850a
Arlolic <ahcharba@gmail.com>
2021-03-31 03:35:29 -0400

    Seed+small changes to schema.

--- schema.sql
+++ schema.sql
@@ -1,10 +1,31 @@
+CREATE TYPE score_type AS ENUM
+('COUNT ASC','COUNT DESC','TIME ASC','TIME DESC','WEIGHT ASC','WEIGHT DESC');
+
 CREATE TABLE athletes(
 	ID int,
 	name varchar(255),
 	date_of_birth date,
 	gender char(1),
-	email varchar(255),
+	email varchar(255) UNIQUE,
 	PRIMARY KEY(id)
+);
+
+CREATE TABLE contacts (
+    	ID int,
+    	email varchar(255) UNIQUE, 
+	phone_number varchar(255),
+	name varchar(255),
+	gender char(1),
+    	PRIMARY KEY (ID)
+);
+
+CREATE TABLE partners (
+    ID int,
+	enterprise_name varchar(255),
+	address varchar(255),
+    contact_id int NOT NULL, 
+    PRIMARY KEY (ID),
+    FOREIGN KEY (contact_id) REFERENCES contacts(ID)
 );
 
 CREATE TABLE competitions (
@@ -13,6 +34,13 @@ CREATE TABLE competitions (
     venue varchar (255),
     start_date_time timestamp,
     end_date_time timestamp, 
+    comp_year int,
+    max_men int,
+    max_women int,
+    partner_id int NOT NULL,
+    contact_id int NOT NULL,
+    FOREIGN KEY (partner_id) REFERENCES partners(ID),
+    FOREIGN KEY (contact_id) REFERENCES contacts(ID),
     PRIMARY KEY (ID)
 );
 
@@ -24,27 +52,6 @@ CREATE TABLE register (
     FOREIGN KEY (athlete_id) REFERENCES athletes(ID)
 );
 
-CREATE TABLE contact (
-    	ID int,
-    	email varchar(255), 
-	phone_number varchar(255),
-	name varchar(255),
-	gender char(1),
-    	PRIMARY KEY (ID)
-);
-
-CREATE TABLE partner (
-    ID int,
-	enterprise_name varchar(255),
-	address varchar(255),
-    contact_id int NOT NULL, 
-    PRIMARY KEY (ID),
-    FOREIGN KEY (contact_id) REFERENCES contact(ID)
-);
-
-CREATE TYPE score_type AS ENUM
-('COUNT ASC','COUNT DESC','TIME ASC','TIME DESC','WEIGHT ASC','WEIGHT DESC');
-
 CREATE TABLE events (
     ID int,
 	event_name varchar(255),
@@ -55,4 +62,16 @@ CREATE TABLE events (
     comp_id int NOT NULL, 
     PRIMARY KEY (ID),
     FOREIGN KEY (comp_id) REFERENCES competitions(ID)
+);
+
+CREATE TABLE results (
+    athlete_id int,
+	event_id int,
+	event_name varchar(255),
+	main_score int NOT NULL,
+	time_capped_score int,
+	tie_breaker_score int,
+    PRIMARY KEY (athlete_id, event_id),
+    FOREIGN KEY (event_id) REFERENCES events(ID),
+    FOREIGN KEY (athlete_id) REFERENCES athletes(ID)
 );
--- seed.sql
+++ seed.sql
@@ -1,10 +1,12 @@
-INSERT INTO athletes(ID, name, date_of_birth, gender)
+INSERT INTO athletes(ID, name, date_of_birth, email, gender)
 VALUES 
-	(1, 'Jojo Lethug', '2000-10-02', 'male'),
-	(2, 'Dio Levillain', '2000-01-02', 'male'),
-	(3, 'Micheline Michelin', '1976-12-12', 'female'),
-	(4, 'Jean-Michel Crapaud', '1992-04-02', 'crapaud'),
-	(5, 'Jackie Chan', '1980-10-03', 'man');
+	(123, 'Jojo Lethug', '2000-10-02','myemailjojo@gmail.com', 'm'),
+	(223, 'Dio Levillain', '2000-01-02','myemaildio@gmail.com' , 'm'),
+	(312, 'Micheline Michelin', '1976-12-12','myemailmich@gmail.com' , 'f'),
+	(313, 'Ginette Legault', '1976-12-11','myemailgine@gmail.com' , 'f'),
+	(322, 'Frederico Martino', '1936-12-12','myemailfred@gmail.com' , 'm'),
+	(413, 'Jean-Michel Crapaud', '1992-04-02','myemailjean@gmail.com' , 'm'),
+	(532, 'Jackie Chan', '1980-10-03','myemailjack@gmail.com' , 'm');
 INSERT INTO competitions(ID, comp_name, venue, start_date_time, end_date_time)
 VALUES 
 	(23, 'Compétition de Rugby', 'Ottawa', '2021-09-28 10:00:00', '2021-09-28 14:00:00'),
